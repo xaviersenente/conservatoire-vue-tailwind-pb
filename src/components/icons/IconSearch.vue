d6d7a813d2417866a8e2d9340b3a6099e31c5214
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2024-03-07 13:16:05 +0100

    etape-05

---
+++ src/components/icons/IconSearch.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="24"
+    height="24"
+    aria-hidden="true"
+    fill="none"
+    stroke="black"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="11" cy="10" r="7" stroke-width="2" />
+    <path stroke-width="2" d="M17.707 17.293l3.536 3.535" />
+  </svg>
+</template>
