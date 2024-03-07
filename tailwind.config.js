6ee9a84405cd3827196f00a8bddd05426edc9b28
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2024-03-07 13:19:31 +0100

    etape-06

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      indigo: '#1d096b',
+      pink: '#feaa9b',
+      white: '#ffffff',
+      black: '#000000',
+      transparent: 'transparent',
+      gray: colors.zinc
+    },
+    fontFamily: {
+      sans: ['Mulish', 'sans-serif'],
+      serif: ['Space Mono', 'monospace']
+    },
     extend: {}
   },
   plugins: []
