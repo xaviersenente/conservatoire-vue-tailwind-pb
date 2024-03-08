a5b80d49fdabee7a63c80381e4b10f4c580ad8c1
Xavier SENENTE <xavier.senente@univ-fcomte.fr>
2024-03-08 11:14:27 +0100

    etape-18

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -31,4 +31,39 @@ import IconDanse from '@/components/icons/IconDanse.vue'
       </nav>
     </template>
   </Hero>
+
+  <section class="grille my-24 space-y-8 lg:grid-rows-2 lg:space-y-0">
+    <img
+      class="lg:col-span-7 lg:col-start-1 lg:row-span-2 lg:row-start-1"
+      src="/img/crd-int-01.jpg"
+      alt="Salle de musique du conservatoire"
+    />
+    <header
+      class="space-y-8 bg-white lg:col-span-7 lg:col-start-6 lg:row-span-1 lg:row-start-1 lg:p-16"
+    >
+      <h2 class="text-xl italic leading-normal lg:text-3xl">
+        Découvrez le Conservatoire, ses enseignements, ses équipes dans un nouveau lieu dédié à la
+        culture artistique.
+      </h2>
+    </header>
+
+    <ul class="grid grid-cols-2 gap-12 lg:col-span-4 lg:col-start-9 lg:row-start-2">
+      <li>
+        <span class="block text-4xl text-pink lg:text-6xl">9</span>
+        <span>sites</span>
+      </li>
+      <li>
+        <span class="block text-4xl text-pink lg:text-6xl">84</span>
+        <span>enseignants</span>
+      </li>
+      <li>
+        <span class="block text-4xl text-pink lg:text-6xl">1500</span>
+        <span>élèves</span>
+      </li>
+      <li>
+        <span class="block text-4xl text-pink lg:text-6xl">50</span>
+        <span>disciplines</span>
+      </li>
+    </ul>
+  </section>
 </template>
